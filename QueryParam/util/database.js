88c7c9bf11cbfe24dd6cf5594d0cf97769a54429
Faranaz08 <faranazannigeri3142@gmail.com>
2024-02-05 16:52:55 +0530

    Delete product

--- QueryParam/util/database.js
+++ QueryParam/util/database.js
@@ -3,8 +3,8 @@ const mysql=require('mysql2');
 const pool=mysql.createPool({
     host:'localhost',
     user:'root',
-    database:'node-complete',
-    password:'noddecomplete'
+    database:'world',
+    password:'faranaz'
 });
 
 module.exports =pool.promise();
